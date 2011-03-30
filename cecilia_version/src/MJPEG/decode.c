5c0811c67d036386be044f81ac524e965c0b8801
Christophe Laferriere <christophe.laferriere@gmail.com>
2011-03-31 00:48:13 +0200

    Add structures and variables

--- cecilia_version/src/MJPEG/decode.c
+++ cecilia_version/src/MJPEG/decode.c
@@ -19,6 +19,13 @@ const uint8_t G_ZZ[64] = {
   53, 60, 61, 54, 47, 55, 62, 63
 };
 
+// TODO : add FRAME_LOOKAHEAD, nb_stream and [3] for YCbCr things (because of
+// the chroma_ss loop
+uint8_t *YCbCr_MCU[3] = { NULL, NULL, NULL};
+uint8_t *YCbCr_MCU_ds[3] = { NULL, NULL, NULL};
+uint32_t *RGB_MCU = NULL;
+
+
 void METHOD(decode, decode)(void *_this, frame_chunk_t* chunks)
 {
   /* to get ((SOF_component[component_index].HV >> 4) & 0xf) now use Streams[video_id].HV */
--- cecilia_version/src/MJPEG/fetch.c
+++ cecilia_version/src/MJPEG/fetch.c
@@ -10,14 +10,24 @@ DECLARE_DATA{
 // Global stream table :
 stream_info_t* Streams;
 
-// Global frame achievement table (nb_chunk already treated):
+
+// Global frame achievement table (nb_chunk already treated) :
 uint32_t* Achievements[FRAME_LOOKAHEAD];
+
+// Intern declarations :
+int32_t* MCUs[FRAME_LOOKAHEAD][3][64];
+int32_t* unZZ_MCUs[FRAME_LOOKAHEAD][3][64];
 
 int METHOD(entry, main)(void *_this, int argc, char** argv)
 {
   printf ("Main start\n");
 
-  /* TODO : dynamic alloc streams and achievements table. */
+  /* TODO : dynamic alloc :
+   * Streams 
+   * Achievements
+   * MCUs
+   * unZZ_MCUs
+   */
 
   CALL (REQUIRED.resize, resize_init);
   CALL (REQUIRED.render, render_init);
