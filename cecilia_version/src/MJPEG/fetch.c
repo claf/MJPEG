59634397a441802512632c4726650867b1f1cfe4
Christophe Laferriere <christophe.laferriere@gmail.com>
2011-04-04 19:36:21 +0200

    insignificant stuffs

--- cecilia_version/src/MJPEG/fetch.c
+++ cecilia_version/src/MJPEG/fetch.c
@@ -7,16 +7,27 @@ DECLARE_DATA{
 
 #include "cecilia.h"
 
+/* Global definition : */
+
 // Global stream table :
 stream_info_t* Streams;
-
 
 // Global frame achievement table (nb_chunk already treated) :
 uint32_t* Achievements[FRAME_LOOKAHEAD];
 
-// Intern declarations :
+/* Intern declarations : */
+
 int32_t* MCUs[FRAME_LOOKAHEAD][3][64];
 int32_t* unZZ_MCUs[FRAME_LOOKAHEAD][3][64];
+
+/* Intern functions : */
+
+static void usage(char *progname) {
+	printf("Usage : %s [options] <mjpeg_file>\n", progname);
+	printf("Options list :\n");
+	printf("\t-f <framerate> : set framerate for the movie\n");
+	printf("\t-h : display this help and exit\n");
+}
 
 int METHOD(entry, main)(void *_this, int argc, char** argv)
 {
@@ -29,6 +40,7 @@ int METHOD(entry, main)(void *_this, int argc, char** argv)
    * unZZ_MCUs
    */
 
+  CALL (REQUIRED.decode, decode_init); //?
   CALL (REQUIRED.resize, resize_init);
   CALL (REQUIRED.render, render_init);
   CALLMINE (fetch, fetch);
