f262494a76ef5a1de3089cdedd550e1f472bd5f1
Christophe Laferriere <christophe.laferriere@gmail.com>
2011-07-08 16:37:50 +0200

    Some changes, not really relevant

--- cecilia_version/xkaapi_src/env.sh
+++ cecilia_version/xkaapi_src/env.sh
@@ -1,6 +1,5 @@
-export LD_LIBRARY_PATH=/home/claferri/opt/rls/lib
+#export LD_LIBRARY_PATH=/home/claferri/opt/rls/lib
 export CFLAGS="-D_FETCH_DEBUG -D_XKAAPI_DEBUG"
 export SDL_VIDEODRIVER=dummy
-export KAAPI_DISPLAY_PERF=1
-export KAAPI_STACKSIZE=10485760 #10MBytes
+#export KAAPI_DISPLAY_PERF=1
 #export KAAPI_CPUCOUNT=3
